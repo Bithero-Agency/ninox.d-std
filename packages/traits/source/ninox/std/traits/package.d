44e7d4d07afdc4a353cfe12e9d4793ecbfecb2ae
Mai-Lapyst <hello@lapyst.dev>
2024-07-21 15:56:09 +0200

    Add BuildFnType and BuildDgType to better build function/delegate types from template args (including storage classes like `ref`)

--- packages/traits/source/ninox/std/traits/package.d
+++ packages/traits/source/ninox/std/traits/package.d
@@ -78,3 +78,123 @@ unittest {
     static assert (BuildImportCodeForType!(Test[string]) == "imported!\"ninox.std.traits\".__unittest_L73_C1.Test[immutable(char)[]]");
     static assert (BuildImportCodeForType!(Test[]) == "imported!\"ninox.std.traits\".__unittest_L73_C1.Test[]");
 }
+
+/** 
+ * Helper to mark an type `T` as an `ref T`.
+ * 
+ * Used in `ninox.std.traits.BuildTypeStr`.
+ * 
+ * Params:
+ *   T = The type to use.
+ */
+template RefT(alias T) {
+    alias InnerT = T;
+}
+
+/** 
+ * Helper to mark an type `T` as an `out T`.
+ * 
+ * Used in `ninox.std.traits.BuildTypeStr`.
+ * 
+ * Params:
+ *   T = The type to use.
+ */
+template OutT(alias T) {
+    alias InnerT = T;
+}
+
+/** 
+ * Helper to mark an type `T` as an `lazy T`.
+ * 
+ * Used in `ninox.std.traits.BuildTypeStr`.
+ * 
+ * Params:
+ *   T = The type to use.
+ */
+template LazyT(alias T) {
+    alias InnerT = T;
+}
+
+/** 
+ * Helper to build a string representation of an type `T`.
+ * 
+ * Note that:
+ *  - instances of `ninox.std.traits.RefT!T` are converted to `ref T`
+ *  - instances of `ninox.std.traits.OutT!T` are converted to `out T`
+ *  - instances of `ninox.std.traits.LazyT!T` are converted to `lazy T`
+ * 
+ * Params:
+ *   T = The type to build a string representation of.
+ */
+template BuildTypeStr(alias T)
+{
+    import std.traits : isInstanceOf;
+    import ninox.std.traits : RefT, OutT, LazyT;
+    static if (isInstanceOf!(RefT, T)) {
+        enum BuildTypeStr = "ref " ~ T.InnerT.stringof;
+    } else static if (isInstanceOf!(OutT, T)) {
+        enum BuildTypeStr = "out " ~ T.InnerT.stringof;
+    } else static if (isInstanceOf!(LazyT, T)) {
+        enum BuildTypeStr = "lazy " ~ T.InnerT.stringof;
+    } else {
+        enum BuildTypeStr = T.stringof;
+    }
+}
+
+unittest {
+    assert(BuildTypeStr!int == "int");
+    assert(BuildTypeStr!(RefT!int) == "ref int");
+    assert(BuildTypeStr!(OutT!int) == "out int");
+    assert(BuildTypeStr!(LazyT!int) == "lazy int");
+}
+
+private template BuildParamStr(ParamsT...)
+{
+    import std.meta : staticMap;
+    import std.string : join;
+    enum BuildParamStr = [ staticMap!(BuildTypeStr, ParamsT) ].join(",");
+}
+
+/** 
+ * Builds a function type from a return type and zero or more parameter types.
+ * If any given type is an instance of `ninox.std.traits.Ref`, a `ref T` is emitted.
+ * 
+ * Params:
+ *   RetT = The returntype of the function.
+ *   ParamsT = The parameter types of the function.
+ */
+template BuildFnType(RetT, ParamsT...) {
+    import ninox.std.traits : BuildParamStr;
+    alias BuildFnType = mixin(
+        BuildTypeStr!RetT ~ " function(" ~ BuildParamStr!ParamsT ~ ")"
+    );
+}
+
+unittest {
+    assert(is(BuildFnType!(void, int) == void function(int)));
+    assert(is(BuildFnType!(void, RefT!int) == void function(ref int)));
+    assert(is(BuildFnType!(void, OutT!int) == void function(out int)));
+    assert(is(BuildFnType!(void, LazyT!int) == void function(lazy int)));
+}
+
+/** 
+ * Builds a delegate type from a return type and zero or more parameter types.
+ * If any given type is an instance of `ninox.std.traits.Ref`, a `ref T` is emitted.
+ * 
+ * Params:
+ *   RetT = The returntype of the delegate.
+ *   ParamsT = The parameter types of the delegate.
+ */
+template BuildDgType(RetT, ParamsT...) {
+    import ninox.std.traits : BuildParamStr;
+    alias BuildDgType = mixin(
+        BuildTypeStr!RetT ~ " delegate(" ~ BuildParamStr!ParamsT ~ ")"
+    );
+}
+
+unittest {
+    assert(is(BuildDgType!(void, int) == void delegate(int)));
+    assert(is(BuildDgType!(void, RefT!int) == void delegate(ref int)));
+    assert(is(BuildDgType!(void, OutT!int) == void delegate(out int)));
+    assert(is(BuildDgType!(void, LazyT!int) == void delegate(lazy int)));
+}
