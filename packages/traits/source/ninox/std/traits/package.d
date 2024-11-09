d57d428ea6eb761ca85ac0952fa492e0bd3a0aad
Mai-Lapyst <hello@lapyst.dev>
2024-11-09 15:07:04 +0100

    Update copyright headers

--- packages/traits/source/ninox/std/traits/package.d
+++ packages/traits/source/ninox/std/traits/package.d
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Mai-Lapyst
+ * Copyright (C) 2023-2024 Mai-Lapyst
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
